6fc6f8565f395b3c6e6f3413e8e5b75c0820295c
iamfreakin <qjaqjaqja1877@gmail.com>
2026-01-01 22:28:23 +0900

    feat: Add basic multiplayer host and join flow

--- Source/UdemyTest/PuzzlePlatformsGameInstance.cpp
+++ Source/UdemyTest/PuzzlePlatformsGameInstance.cpp
@@ -12,3 +12,29 @@ void UPuzzlePlatformsGameInstance::Init()
 {
 	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"))
 }
+
+void UPuzzlePlatformsGameInstance::Host() 
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/FirstPerson/Lvl_FirstPerson?listen");
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
